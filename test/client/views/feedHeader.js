7f546c10d6bb795a3a4cec6f679cffc72d0d7da6
Devin Dreszer <devindreszer@gmail.com>
2015-01-14 09:44:12 -0700

    FeedHeaderView has passing tests

--- test/client/views/feedHeader.js
+++ test/client/views/feedHeader.js
@@ -1,6 +1,9 @@
 // Load modules
 
-var FeedHeader = require('../../../source/js/views/feedHeader');
+var Backbone = require('backbone');
+var sinon = require('sinon');
+
+var FeedHeaderView = require('../../../source/js/views/feedHeader');
 
 
 // Declare internals
@@ -10,9 +13,24 @@ var internals = {};
 
 describe('FeedHeader', function () {
 
+    var fakeModel, triggerSpy, view;
+
+    beforeEach(function() {
+
+        fakeModel = new Backbone.Model();
+        view = new FeedHeaderView({ model: fakeModel });
+        view.render();
+        triggerSpy = sinon.spy(view, 'trigger');
+    });
+
     describe('when the collapse all action is clicked', function () {
 
-        it('all expanded requests get collapsed');
+        it('all expanded requests get collapsed', function() {
+
+            view.$el.find('.expander').click();
+
+            expect(triggerSpy).to.have.been.calledWith('collapseAll');
+        });
 
     });
 
@@ -20,9 +38,25 @@ describe('FeedHeader', function () {
 
         describe('when the favorites filter action is clicked', function () {
 
-            it('displays the favorites filter as turned on');
+            var $favorite;
 
-            it('triggers "toggleFavorites" with the toggle set to true');
+            beforeEach(function() {
+
+                $favorite = view.$el.find('.favorite');
+                view.enableFavoritesFilter();
+                $favorite.click();
+
+            });
+
+            it('displays the favorites filter as turned on', function() {
+
+                expect($favorite).to.have.class('active');
+            });
+
+            it('triggers "toggleFavorites" with the toggle set to true', function() {
+
+                expect(triggerSpy).to.have.been.calledWith('toggleFavorites', true);
+            });
 
         });
 
@@ -32,9 +66,27 @@ describe('FeedHeader', function () {
 
         describe('when the favorites filter action is clicked', function () {
 
-            it('displays the favorites filter as turned off');
+            var $favorite;
 
-            it('triggers "toggleFavorites" with the toggle set to false');
+            beforeEach(function() {
+
+                $favorite = view.$el.find('.favorite');
+                view.enableFavoritesFilter();
+                $favorite.click();
+
+                triggerSpy.reset();
+                $favorite.click();
+            });
+
+            it('displays the favorites filter as turned off', function() {
+
+                expect($favorite).to.not.have.class('active');
+            });
+
+            it('triggers "toggleFavorites" with the toggle set to false', function() {
+
+                expect(triggerSpy).to.have.been.calledWith('toggleFavorites', false);
+            });
 
         });
 
@@ -42,33 +94,75 @@ describe('FeedHeader', function () {
 
     describe('#clear', function () {
 
-        it('disables the favorites filter action');
+        it('disables the favorites filter action', function() {
 
-        it('disables the collapse all action');
+            var disableFavoritesFilterSpy = sinon.spy(view, 'disableFavoritesFilter');
+
+            view.clear();
+
+            expect(disableFavoritesFilterSpy).to.have.been.called;
+        });
+
+        it('disables the collapse all action', function() {
+
+            var disableCollapseAllSpy = sinon.spy(view, 'disableCollapseAll');
+
+            view.clear();
+
+            expect(disableCollapseAllSpy).to.have.been.called;
+        });
 
     });
 
     describe('#enableCollapseAll', function () {
 
-        it('enables the collapse all action');
+        it('enables the collapse all action', function() {
+
+            var $expander = view.$el.find('.expander');
+
+            view.enableCollapseAll();
+
+            expect($expander).to.have.class('expanded');
+        });
 
     });
 
     describe('#disableCollapseAll', function () {
 
-        it('disables the collapse all action');
+        it('disables the collapse all action', function() {
+
+            var $expander = view.$el.find('.expander');
+
+            view.disableCollapseAll();
+
+            expect($expander).to.not.have.class('expanded');
+        });
 
     });
 
     describe('#enableFavoritesFilter', function () {
 
-        it('enables the favorited filter action');
+        it('enables the favorited filter action', function() {
+
+            var $favorite = view.$el.find('.favorite');
+
+            view.enableFavoritesFilter();
+
+            expect($favorite).to.have.class('enabled');
+        });
 
     });
 
     describe('#disableFavoritesFilter', function () {
 
-        it('disables the favorited filter action');
+        it('disables the favorited filter action', function() {
+
+            var $favorite = view.$el.find('.favorite');
+
+            view.disableFavoritesFilter();
+
+            expect($favorite).to.not.have.class('enabled');
+        });
 
     });
 
